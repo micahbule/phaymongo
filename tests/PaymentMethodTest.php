4fd4edb0feab5e69538a64790ba1aebf4a878777
Micah Andrew F. Bule <micah.bule@gmail.com>
2023-01-11 19:53:16 +0800

    Feat payment method test (#13)
    * chore: add test script to composer
    
    * chore: remove version number
    
    * chore: update lockfile
    
    * chore: run unit test
    
    * feat: add unit tests for payment method class

---
+++ tests/PaymentMethodTest.php
@@ -0,0 +1,74 @@
+<?php
+
+it('can create a payment method without optional fields', function () {
+    $dummyAttributes = array(
+        'type' => 'gcash',
+    );
+
+    $dummyPayload = array(
+        'data' => array(
+            'attributes' => $dummyAttributes,
+        ),
+    );
+
+    $utilsMock = mock('alias:Paymongo\Phaymongo\PaymongoUtils');
+    $utilsMock->shouldReceive('constructPayload')
+        ->withArgs([$dummyAttributes])
+        ->atLeast()
+        ->times(1)
+        ->andReturn($dummyPayload);
+
+    $paymentMethodMock = mock('Paymongo\Phaymongo\PaymentMethod')->makePartial();
+    $paymentMethodMock->shouldReceive('createResource')
+        ->withArgs([$dummyPayload])
+        ->atLeast()
+        ->times(1)
+        ->andReturn(['success' => true]);
+
+    $response = $paymentMethodMock->create('gcash');
+
+    expect($response)->toBe(['success' => true]);
+});
+
+it('can create a payment method with optional fields', function () {
+    $dummyAttributes = array(
+        'type' => 'gcash',
+        'details' => ['some' => 'details'],
+        'billing' => ['some' => 'billing'],
+        'metadata' => ['some' => 'data'],
+    );
+
+    $dummyPayload = array(
+        'data' => array(
+            'attributes' => $dummyAttributes,
+        ),
+    );
+
+    $utilsMock = mock('alias:Paymongo\Phaymongo\PaymongoUtils');
+    $utilsMock->shouldReceive('constructPayload')
+        ->withArgs([$dummyAttributes])
+        ->atLeast()
+        ->times(1)
+        ->andReturn($dummyPayload);
+
+    $paymentMethodMock = mock('Paymongo\Phaymongo\PaymentMethod')->makePartial();
+    $paymentMethodMock->shouldReceive('createResource')
+        ->withArgs([$dummyPayload])
+        ->atLeast()
+        ->times(1)
+        ->andReturn(['success' => true]);
+
+    $response = $paymentMethodMock->create('gcash', ['some' => 'details'], ['some' => 'billing'], ['some' => 'data']);
+
+    expect($response)->toBe(['success' => true]);
+});
+
+it('can retrieve a payment method by ID', function () {
+    $paymentMethodMock = mock('Paymongo\Phaymongo\PaymentMethod')->makePartial();
+    $paymentMethodMock->shouldReceive('retrieveResourceById')
+        ->withArgs([1])
+        ->atLeast()
+        ->times(1);
+    
+    $paymentMethodMock->retrieveById(1);
+});
